0b318f6e3f1d6cdb2dc52fc287b3c2b1b92113d2
Johan Blignaut <blignaut.android@gmail.com>
2023-05-24 13:28:50 +0200

    Initial commit.

---
+++ src/main/java/com/johan/blignaut/entelect/vitalityincubator/VitalityIncubatorApplication.java
@@ -0,0 +1,13 @@
+package com.johan.blignaut.entelect.vitalityincubator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalityIncubatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalityIncubatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johan/blignaut/entelect/vitalityincubator/VitalityIncubatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johan.blignaut.entelect.vitalityincubator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalityIncubatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
