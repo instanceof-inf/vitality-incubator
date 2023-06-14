fe79f67f7116e8a22b43505a9beb2468e22c21dd
Johan Blignaut <blignaut.android@gmail.com>
2023-06-14 11:22:23 +0200

    Added text message to exception invocation and added mapper class back in

--- src/main/java/com/johan/blignaut/entelect/vitalityincubator/task2/GeoDBController.java
+++ src/main/java/com/johan/blignaut/entelect/vitalityincubator/task2/GeoDBController.java
@@ -5,6 +5,7 @@ import com.johan.blignaut.entelect.vitalityincubator.restclient.model.GeoDBClien
 import com.johan.blignaut.entelect.vitalityincubator.restserver.api.GeoApi;
 import com.johan.blignaut.entelect.vitalityincubator.restserver.model.GeoDBServer;
 import com.johan.blignaut.entelect.vitalityincubator.task2.exception.CustomInternalServerException;
+import com.johan.blignaut.entelect.vitalityincubator.task2.helpers.GeoDBMappingHelper;
 import com.johan.blignaut.entelect.vitalityincubator.task2.service.GeoDBMappingService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,28 +16,14 @@ import java.io.IOException;
 
 @RestController
 public class GeoDBController implements GeoApi {
+    private final GeoDBMappingHelper geoDBMappingHelper;
 
-    private final GeodbApi geodbApi;
-    private final GeoDBMappingService geoDBMappingService;
-
-    public GeoDBController(GeodbApi geodbApi, GeoDBMappingService geoDBMappingService) {
-        this.geodbApi = geodbApi;
-        this.geoDBMappingService = geoDBMappingService;
+    public GeoDBController(GeoDBMappingHelper geoDBMappingHelper) {
+        this.geoDBMappingHelper = geoDBMappingHelper;
     }
 
     @Override
     public ResponseEntity<GeoDBServer> getPlaces(String filterByName) {
-        try {
-            Call<GeoDBClient> call = geodbApi.getPlaces();
-            Response<GeoDBClient> response = call.execute();
-            if (response.isSuccessful()) {
-                GeoDBClient geodbClient = response.body();
-                GeoDBServer geoDBServer = geoDBMappingService.mapClientToServerGeoDB(geodbClient);
-                return ResponseEntity.ok(geoDBServer);
-            }
-            throw new CustomInternalServerException();
-        } catch (IOException e) {
-            throw new CustomInternalServerException(e);
-        }
+        return ResponseEntity.ok(geoDBMappingHelper.mapClientToServerGeoDB());
     }
 }
